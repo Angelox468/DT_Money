cb7d94dee3ed44f742fe3b033e2a6ae1d9eb4abb
Angelo Ricardo.gonçalves Filho <angelo.ricardo123@hotmail.com>
2022-07-03 11:32:09 -0300

    :tada: aplicação finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import { Header } from "./components/Header/index";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 
 import { GlobalStyle } from "./styles/global";
+import { TransactionsProvider } from "./hooks/useTransactions";
 
 Modal.setAppElement('#root')
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
-
+ 
   function handleOpenNewTransactionModal() {
     setIsNewTransactionModalOpen(true)
   }
@@ -20,7 +21,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
       <Dashboard />
@@ -31,6 +32,6 @@ export function App() {
       />
       
       <GlobalStyle/>
-    </>
+    </TransactionsProvider>
   );
 }
--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -5,6 +5,7 @@ import { Container } from "./styles"
 
 
 export function Dashboard(){
+  
   return (
     <Container>
       <Summary />
